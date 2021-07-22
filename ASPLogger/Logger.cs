df2fee9b9f1498e42398d49ac582d431caa8870e
Adam Schumph <adamschumph@gmail.com>
2021-07-22 10:12:57 -0700

    Add project files.

---
+++ ASPLogger/Logger.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+
+namespace ASPLogger
+{
+    /// <summary>
+    /// A class library to log information to the Console and/or File simulteaneously
+    /// </summary>
+    public class Logger
+    {
+
+        /// <summary>Indicates if logging should be directed to the Console or not</summary>
+        public bool ToConsole { get; set; } = true;
+
+
+        /// <summary>The name of the text file to generate</summary>
+        public string FileName { get; set; } = "LogFile.log";
+        /// <summary>The folder to save the text file in</summary>
+        public string Folder { get; set; } = "./";
+
+        /// <summary>The full path of the text file</summary>
+        public string FullPath => Path.Combine(Folder,FileName);
+
+        private TextWriter textWriter = null;
+        /// <summary>Indicates if logging is currently directed to a text file or not</summary>
+        public bool ToTextFile => textWriter != null;
+
+        public Logger()
+        {
+
+        }
+
+        public void StartLoggingToTextFile()
+        {
+            if (textWriter == null)
+                textWriter = new StreamWriter(FullPath);
+        }
+
+        public void StopLoggingToTextFile()
+        {
+            if (textWriter != null)
+            {
+                textWriter.Close();
+                textWriter = null;
+            }
+        }
+
+        /// <summary>Write a message to the log</summary>
+        public void Write(string message)
+        {
+            if (ToConsole)
+                Console.Write(message);
+
+            if (ToTextFile)
+                textWriter.Write(message);
+        }
+
+        /// <summary>Write a message to the log, followed by an eol marker</summary>
+        public void WriteLine(string message)
+        {
+
+            if (ToConsole)
+                Console.WriteLine(message);
+
+            if (ToTextFile)
+                textWriter.WriteLine(message);
+        }
+
+    }
+}
---
+++ ASPLoggerConsoleApp/Program.cs
@@ -0,0 +1,49 @@
+﻿using ASPLogger;
+using System;
+
+namespace ASPLoggerConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ASP Logger Console App");
+            Console.WriteLine("A simple console app to validate the behaviour of the ASPLogger class.");
+            Console.WriteLine();
+
+            {
+                Console.WriteLine("--------------------------------------------------------------------------------");
+                Console.WriteLine("Testing to Console Only");
+                Logger logger = new();
+                logger.Write("WriteToConsole!");
+                logger.Write("WriteToConsole!");
+                logger.WriteLine("WriteLineToConsole!");
+                logger.WriteLine("WriteLineToConsole!");
+                Console.WriteLine();
+            }
+
+            {
+                Console.WriteLine("--------------------------------------------------------------------------------");
+                Console.WriteLine("Testing to File and Console");
+                Logger logger = new();
+                Console.WriteLine($"Output to: {logger.FullPath}");
+
+                logger.StartLoggingToTextFile();
+                logger.Write("WriteToFileAndConsole!");
+                logger.Write("WriteToFileAndConsole!");
+                logger.WriteLine("WriteToFileAndConsole!");
+                logger.WriteLine("WriteToFileAndConsole!");
+                logger.StopLoggingToTextFile();
+
+                Console.WriteLine();
+                Console.WriteLine($"Displaying contents of {logger.FullPath}...");
+
+                string[] lines = System.IO.File.ReadAllLines(logger.FullPath);
+                foreach (string line in lines)
+                    Console.WriteLine(line);
+
+            }
+
+        }
+    }
+}
